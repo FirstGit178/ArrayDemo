a7402840b45c18d6f09c085f1b2f608643fd01bb
Fergal McNamara <g00180082@atu.ie>
2025-02-08 23:10:23 +0000

    Array List add 100

--- src/main/java/ie/atu/arrays/ArrayDemo.java
+++ src/main/java/ie/atu/arrays/ArrayDemo.java
@@ -1,22 +1,17 @@
 package ie.atu.arrays;
 
+import java.util.ArrayList;
+
 public class ArrayDemo {
     public static void main(String[] args) {
-        int[] myNumbers = new int[10];
-        myNumbers[0] = 100;
-        myNumbers[1] = 200;
-        myNumbers[2] = 300;
-        myNumbers[3] = 400;
-        myNumbers[4] = 500;
-        myNumbers[5] = 600;
-        myNumbers[6] = 700;
-        myNumbers[7] = 800;
-        myNumbers[8] = 900;
-        myNumbers[9] = 1000;
+       ArrayList<Integer> list = new ArrayList<Integer>();
+       list.add(100);
+       list.add(200);
+       list.add(300);
 
-        for(int i = 0; i < myNumbers.length; i++)
-        {
-            System.out.println(myNumbers[i]);
-        }
+       for(Integer i : list)
+       {
+           System.out.println("The value is " + i);
+       }
     }
 }
